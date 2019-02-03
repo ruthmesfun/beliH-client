de57a68f40fb62902a19ecfa5ea8bdd48a2ba2fc
ruthmesfun <ruth.mesfun@gmail.com>
2019-02-03 12:58:00 -0500

    Lesson is clicked once completed

---
+++ src/containers/LessonInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Lesson from '../components/Lesson'
+
+class LessonInfo extends Component {
+    render(){
+        return (
+            <div>
+            lesson info
+               <Lesson />
+            </div>
+        )
+    }
+}
+
+export default LessonInfo
